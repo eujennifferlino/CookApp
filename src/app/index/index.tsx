ae7b457642c2f9822be9c450a7ee14a9f62ee2ec
eujennifferlino <eujennifferlino@gmail.com>
2024-03-07 16:28:16 -0300

    feat: add selection functionality

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,8 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 
 import { styles } from "./styles";
+import { Ingredient } from "@/components/Ingredient";
+import { useState } from "react";
 
 export default function Index() {
+  const [selected, setSelected] = useState<string[]>([]);
+
+  function handleToggleSelected(value: string) {
+    if (selected.includes(value)) {
+      return setSelected((state) => state.filter((item) => item !== value));
+    }
+
+    setSelected((state) => [...state, value]);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
@@ -13,6 +25,21 @@ export default function Index() {
       <Text style={styles.message}>
         Descubra as receitas com base nos ingredientes que você escolheu!
       </Text>
+
+      <ScrollView
+        contentContainerStyle={styles.ingredients}
+        showsVerticalScrollIndicator={false}
+      >
+        {Array.from({ length: 100 }).map((item, index) => (
+          <Ingredient
+            key={index}
+            name="Tomate"
+            image=""
+            selected={selected.includes(String(index))}
+            onPress={() => handleToggleSelected(String(index))}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 }
